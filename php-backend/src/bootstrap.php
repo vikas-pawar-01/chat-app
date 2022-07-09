0139afdb046e5367636d520c99de7ffc577932b7
Vikas Pawar <vicky.pawar17@gmail.com>
2022-07-09 12:50:22 +0530

    Update bootstrap.php

--- php-backend/src/bootstrap.php
+++ php-backend/src/bootstrap.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 require 'vendor/autoload.php';
 
@@ -7,14 +6,4 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable( __DIR__ );
 $dotenv->load();
 
-=======
-<?php
-require 'vendor/autoload.php';
-
-use Dotenv\Dotenv;
-
-$dotenv = Dotenv::createImmutable( __DIR__ );
-$dotenv->load();
-
->>>>>>> e5cc454edf8ece10371a8aa2c2142769c5584654
 $objDatabaseConnection = ( new Src\System\DatabaseConnector() )->getConnection();
